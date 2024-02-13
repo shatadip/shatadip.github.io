9d444b7b8b9a745ed280836a956ea7f072c19734
shatadip <shatadip.majumder@gmail.com>
2024-02-14 05:18:35 +0530

    github actions workflow patch 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+}
+module.exports = nextConfig
