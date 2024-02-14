5259df6f510a69edaae20539d6c1c1c6c3d8695b
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-02-15 01:29:04 +0530

    Patch  7

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
 
