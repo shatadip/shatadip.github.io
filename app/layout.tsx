14e7dd553f119b06cb48880d7dffde1c0cdd9e75
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-03-22 07:09:30 +0530

    Added ogimage, updated metadata in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,30 @@ const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shatadip Majumder | Coder + Teacher",
-  description: "Hi, I am Shatadip, I code and I teach. I am open to new opportunities.",
+  description: "I am Shatadip Majumder, an experienced Full Stack Developer and Online Instructor. Launched 6 online courses on Udemy and Skillshare, covering Tech and Productivity topics. Delivered over 250 mid to large-scale projects, including 100+ web apps. I am also the former founder of Borofy, a popular e-commerce platform.",
+  generator: 'Next.js',
+  applicationName: 'Shatadip Majumder',
+  referrer: 'origin-when-cross-origin',
+  keywords: ["Shatadip Majumder", "Full Stack Developer", "Online Instructor", "Tech", "Productivity", "MERN", "LAMP", "Django Stack", " Git", " GitHub", " React Native", "Chrome Manifest V3", "BASH", "AWS", "GCP", "Azure", "OCI", "Vercel", "Instructional Design", "Adaptability", "Analytical Thinking"],
+  authors: [{ name: 'Shatadip Majumder', url: 'https://shatadip.com' }],
+  creator: 'Shatadip Majumder',
+  publisher: 'Shatadip Majumder',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL('https://shatadip.com'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+    },
+  },
+  openGraph: {
+    images: 'images/ogimage.png',
+  }
 };
 
 export default function RootLayout({
